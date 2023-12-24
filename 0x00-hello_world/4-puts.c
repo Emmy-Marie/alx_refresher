6c9ad90e2b70832747f1448d3e04d4c3e8c108cd
Nnaemeka Osuagwu-Nwogu <Nnaemeka.osuagwu.n@gmail.com>
2023-12-24 11:26:06 +0000

    A program that prints a string using the puts() function. README.md file also created

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A simple program that prints a string using the puts() function
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
