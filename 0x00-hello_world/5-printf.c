4c05fa49687eee009fdc9bf8726f7189ae4ba0b6
Nnaemeka Osuagwu-Nwogu <Nnaemeka.osuagwu.n@gmail.com>
2024-01-01 10:19:11 +0000

    A program that prints a string using the printf() function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A simple program that prints a string using the printf() function.
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
